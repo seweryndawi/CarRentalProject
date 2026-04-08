75d38052f4fbe164b28ae82be720edc9f60cf9c7
unknown <seweryndawi@gmail.com>
2026-04-08 16:37:13 +0200

    Added swagger and improved README

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using CarRental.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<CarRentalContext>();
 builder.Services.AddScoped<CarService>();
 
@@ -12,7 +15,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 //app.UseHttpsRedirection();
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## Description
-ASP.NET Core Web API for managing car rentals.
+ASP.NET Core Web API that allows renting cars. Has options of
+- getting selected or all cars
+- adding new cars
+- deleting existing ones
+- updating existing ones
 
 ## Tech Stack
 ASP.NET Core
+EF Core Sqlite
